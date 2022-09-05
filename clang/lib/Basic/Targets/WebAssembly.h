f8be30ca1749f461bce7cab778a1e33fe1794fe4
Evan Johnson <e5johnso@eng.ucsd.edu>
2022-09-05 07:34:29 -0700

    update Data layout

--- clang/lib/Basic/Targets/WebAssembly.h
+++ clang/lib/Basic/Targets/WebAssembly.h
@@ -162,7 +162,7 @@ public:
                                    const TargetOptions &Opts)
       : WebAssemblyTargetInfo(T, Opts) {
     // the following assumes CapabilityABI, and the equivalent of RISCV's "+xcheri" feature
-    resetDataLayout("e-m:e-p:32:32-i64:64-n32:64-S128-pf200:64:64:64:32-A200-P0-G200");
+    resetDataLayout("e-m:e-p:32:32-i64:64-n32:64-S128-pf200:128:128:128:32-A200-P0-G200");
   }
 
 protected:
